9eaa7a6d908a25277143e41ae0400ad5e41caf9e
Shashanka Dash <56248556+SHASHANKADASH@users.noreply.github.com>
2026-08-02 17:23:57 +0530

    [bp-3] (#9)
    * [bp-3]
    
    Fixed payments and account service to run them as separate microservices

--- account-service/src/main/java/org/shashanka/AccountsApplication.java
+++ account-service/src/main/java/org/shashanka/AccountsApplication.java
@@ -2,15 +2,14 @@ package org.shashanka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class PaymentFraudDetectionServiceApplication {
+public class AccountsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentFraudDetectionServiceApplication.class, args);
+		SpringApplication.run(AccountsApplication.class, args);
 	}
 
 }
