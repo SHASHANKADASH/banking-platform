b98f0ede92bb7b813c374a39056e0fa5e318c0dd
SHASHANKADASH <shibutom216@gmail.com>
2026-08-02 18:52:28 +0530

    [bp-4]
    Moved fraud check code to fraud package

---
+++ payment-service/src/test/java/org/shashanka/PaymentsApplicationTests.java
@@ -0,0 +1,11 @@
+package org.shashanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaymentsApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
