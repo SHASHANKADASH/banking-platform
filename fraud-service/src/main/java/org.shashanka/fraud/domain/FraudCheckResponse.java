289cb6faee24c21a5dcbdb3c46f0df3b0531521d
SHASHANKADASH <shibutom216@gmail.com>
2026-08-08 18:36:06 +0530

    [bp-6]
    Updated infra code to integrate fraud service

--- fraud-service/src/main/java/org.shashanka/fraud/domain/FraudCheckResponse.java
+++ fraud-service/src/main/java/org.shashanka/fraud/domain/FraudCheckResponse.java
@@ -1,10 +1,12 @@
 package org.shashanka.fraud.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @AllArgsConstructor
+@Getter
 public class FraudCheckResponse {
     Boolean approved;
     String reason;
